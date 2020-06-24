cf6e9aa77d8f4945fb9c63ea86629cd73b552567
Yash Mahalwal <yashmahalwal@gmail.com>
2020-06-25 03:43:11 +0530

    feat(reset password): completed flow

--- src/graphql/resetPassword.ts
+++ src/graphql/resetPassword.ts
@@ -1,12 +1,14 @@
-import { Resolver, Mutation, Arg } from "type-graphql";
+import { Resolver, Mutation, Arg, InputType, Field } from "type-graphql";
 import { Empty } from "./types/basic";
 import { AuthServiceClient } from "../megatreopuz-protos/auth_grpc_pb";
 import { credentials } from "grpc";
 import {
     SendPasswordRecoveryEmailRequest,
     Empty as grpcEmpty,
+    UpdatePasswordRequest,
 } from "../megatreopuz-protos/auth_pb";
 import { makeRPCCall } from "../utils/handleUnaryGrpc";
+
 @Resolver()
 export class ResetPassword {
     @Mutation((returns) => Empty)
@@ -21,6 +23,28 @@ export class ResetPassword {
         await makeRPCCall<grpcEmpty, SendPasswordRecoveryEmailRequest>(
             client,
             client.sendPasswordRecoveryEmail,
+            request
+        );
+
+        return new Empty();
+    }
+
+    @Mutation((returns) => Empty)
+    async updatePassword(
+        @Arg("code") code: string,
+        @Arg("newPassword") newPassword: string
+    ): Promise<Empty> {
+        const client = new AuthServiceClient(
+            process.env.AUTH_GRPC_SERVER ?? "",
+            credentials.createInsecure()
+        );
+
+        const request = new UpdatePasswordRequest();
+        request.setNewpassword(newPassword);
+        request.setPasswordid(code);
+        await makeRPCCall<grpcEmpty, UpdatePasswordRequest>(
+            client,
+            client.updatePassword,
             request
         );
 
